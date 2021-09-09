b0fc826838f55c6098ac3d85fcca662976a4f41f
Овчаренко Андрей <ia@krot.name>
2021-09-09 11:00:08 +0300

    Update init.sql

--- src/main/java/name/krot/dz6/init.sql
+++ src/main/java/name/krot/dz6/init.sql
@@ -1,4 +1,6 @@
 drop table if exists Buyer;
 drop table if exists Goods;
+create table Buyer (id bigint not null auto_increment, name varchar(255), primary key (id)) engine=InnoDB;
 create table Goods (id bigint not null auto_increment, price double precision not null, title varchar(255), primary key (id)) engine=InnoDB;
-insert into Goods (price, title) values (10, 'asfd');
+insert into Goods (price, title) values (10, 'asfd1');
+insert into Goods (price, title) values (10, 'asfd2');
